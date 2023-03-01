76ef396b362d1615b70e8848b82dda39ec9a6ca9
nicolasmariscal9 <nicolasmariscal9@gmail.com>
2023-02-28 21:43:28 -0300

    primer commit

---
+++ js/function.js
@@ -0,0 +1,19 @@
+document.getElementById ("boton").onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById ("demo").innerHTML = "Trabajo en en conjunto tanto con mis pares, superiores y subordinados"
+}
+
+document.getElementById ("boton2").onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById ("demo2").innerHTML = "Me adapto rapidamente a cualquier clase de entorno"
+}
+
+document.getElementById ("boton3").onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById ("demo3").innerHTML = "Siempre actuo antes de esperar que los problemas lleguen"
+}
+
+document.getElementById ("boton4").onclick = function (){
+    console.log("capturamos el evento click");
+    document.getElementById ("demo4").innerHTML = "Me solidarizo con mi equipo de trabajo para realizar objetivos comunes"
+}
